942f344b63ca2a20bcdf0081add6c4a7b8a46498
Rodrigo Nunes <rodrigo.ribeiro@wconnect.com.br>
2025-02-14 11:15:27 -0300

    change port

--- app.py
+++ app.py
@@ -6,15 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     """Endpoint raiz com informações pessoais"""
-    # Dados que você quer exibir na página
     user_data = {
         "name": "Rodrigo Nunes Sampaio Ribeiro",
         "email": "rnsribeiro@gmail.com",
         "linkedin": "https://www.linkedin.com/in/rodrigo-nsribeiro/"
     }
-
-    # Passando os dados para o template
     return render_template('index.html', user_data=user_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 80))  # Define a porta como 80
+    app.run(host="0.0.0.0", port=port, debug=True)
